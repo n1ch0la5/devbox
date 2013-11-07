91195073faf8e072d7ea0fa841a23dd17fd78836
Ivan Jankovic <ivan@isur.net>
2013-11-07 01:54:40 +0100

    Added support for GD image library

--- modules/php/manifests/init.pp
+++ modules/php/manifests/init.pp
@@ -9,7 +9,7 @@ class php {
         require => Exec['add_repo']
     }
     
-    $packages = ['php5', 'php5-mcrypt', 'php-xml-parser', 'php5-xdebug', 'php5-mysql', 'php5-cli', 'php5-curl', 'php5-fpm', 'libssh2-1-dev', 'php-apc', 'php-pear']
+    $packages = ['php5', 'php5-mcrypt', 'php-xml-parser', 'php5-xdebug', 'php5-mysql', 'php5-cli', 'php5-curl', 'php5-fpm', 'libssh2-1-dev', 'php-apc', 'php-pear', 'php5-gd']
     package { $packages:
         ensure => latest,
         require => Exec['update_repo']
